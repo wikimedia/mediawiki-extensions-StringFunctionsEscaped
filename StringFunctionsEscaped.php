81c0ad4dd9ead4d2cf14fa9e5e1f1b77784494d6
C. Scott Ananian <cscott@cscott.net>
2021-02-18 18:02:39 -0500

    Replace use of Parser::$mFunctionHooks, deprecated in 1.35
    The replacement, Parser::getFunctionHooks(), has been in MediaWiki for
    over a decade (since 1.10.0).
    
    Bug: T275160
    Bug: T275173
    Change-Id: I5ae19465561b150ee1c74a1fe03fa359964e81c4

--- StringFunctionsEscaped.php
+++ StringFunctionsEscaped.php
@@ -1,9 +1,13 @@
 <?php
+use MediaWiki\Extensions\ParserFunctions as PF;
+
 if ( !defined( 'MEDIAWIKI' ) )
-	#die( 'StringFunctionsEscaped::This file is a MediaWiki extension, it is not a valid entry point' );
-if ( !class_exists('ExtStringFunctions',false) && 
-	 !(class_exists('ParserFunctions_HookStub',false) && isset($wgPFEnableStringFunctions) && $wgPFEnableStringFunctions))
-	#die( 'StringFunctionsEscaped::You must have extension StringFunctions or extension ParserFunctions with string functions enabled' );
+	die( 'StringFunctionsEscaped::This file is a MediaWiki extension, it is not a valid entry point' );
+if ( !( class_exists( PF::class, false ) ) ) {
+	// Commented out because this (unmaintained) extension isn't configured
+	// in Wikimedia CI with the proper extension dependencies. (T275173)
+	# die( 'StringFunctionsEscaped::You must have extension ParserFunctions' );
+}
 /*
 
  Defines a superset of string parser functions that allow character escaping in the 'search for' and 'replace with' arguments.
@@ -91,9 +95,7 @@ class ExtStringFunctionsEscaped {
 	 * Note: The needle is limited to specific length.
 	 */
 	public static function runPos_e ( &$parser, $inStr = '', $inNeedle = '', $inOffset = 0 ) {
-		list( $callback, $flags ) = $parser->mFunctionHooks['pos'];
-		return @call_user_func_array( $callback,
-			array_merge( array( $parser ), array( $inStr, stripcslashes( $inNeedle ), $inOffset ) ) );
+		return PF::runPos( $parser, $inStr, stripcslashes( $inNeedle ), $inOffset );
 	}
 
 	/**
@@ -103,9 +105,7 @@ class ExtStringFunctionsEscaped {
 	 * Note: The needle is limited to specific length.
 	 */
 	public static function runRPos_e( &$parser , $inStr = '', $inNeedle = '' ) {
-		list( $callback, $flags ) = $parser->mFunctionHooks['rpos'];
-		return @call_user_func_array( $callback,
-			array_merge( array( $parser ), array( $inStr, stripcslashes( $inNeedle ) ) ) );
+		return PF::runRPos( $parser, $inStr, stripcslashes( $inNeedle ) );
 	}
 
 	/**
@@ -113,9 +113,22 @@ class ExtStringFunctionsEscaped {
 	 * Note: Length of the resulting string is limited.
 	 */
 	public static function runPad_e( &$parser, $inStr = '', $inLen = 0, $inWith = '', $inDirection = '' ) {
-		list( $callback , $flags ) = $parser->mFunctionHooks['pad'];
-		return @call_user_func_array( $callback,
-			array_merge( array( $parser ), array( $inStr, $inLen, stripcslashes( $inWith ), $inDirection ) ) );
+		switch ($inDirection) {
+		default:
+		case 'left':
+			return CoreParserFunctions::padleft( $parent, $inStr, $inLen, stripcslashes($inWith) ?: ' ' );
+		case 'right':
+			return CoreParserFunctions::padright( $parent, $inStr, $inLen, stripcslashes($inWith) ?: ' ' );
+		case 'center':
+			$amt = ($inLen - strlen( $inStr )) / 2;
+			$amt = strlen($inStr) + ($amt < 0 ? 0 : $amt);
+			return self::runPad_e(
+				$parser,
+				self::runPad_e( $parser, $inStr, $amt, $inWith, 'left'),
+				$inLen,
+				$inWith
+			);
+		}
 	}
 
 	/**
@@ -124,10 +137,8 @@ class ExtStringFunctionsEscaped {
 	 * Note: The needle is limited to specific length.
 	 * Note: The product is limited to specific length.
 	 */
-	public static function runReplace_e( &$parser, $inStr = '', $inReplaceFrom = '', $inReplaceTo = '' ) {
-		list( $callback, $flags ) = $parser->mFunctionHooks['replace'];
-		return @call_user_func_array( $callback,
-			array_merge( array( $parser ), array( $inStr, stripcslashes( $inReplaceFrom ), stripcslashes( $inReplaceTo ) ) ) );
+	public static function runReplace_e( &$parser, $inStr = '', $inReplaceFrom = '', $inReplaceTo = '', $inLimit = -1 ) {
+		return PF::runReplace( $parser, $inStr, stripcslashes( $inReplaceFrom ), stripcslashes( $inReplaceTo ), $inLimit );
 	}
 
 	/**
@@ -137,10 +148,8 @@ class ExtStringFunctionsEscaped {
 	 * Note: The divider is limited to specific length.
 	 * Note: Empty string is returned, if there is not enough exploded chunks.
 	 */
-	public static function runExplode_e( &$parser, $inStr = '', $inDiv = '', $inPos = 0 ) {
-		list( $callback, $flags ) = $parser->mFunctionHooks['explode'];
-		return @call_user_func_array( $callback,
-			array_merge(array( $parser ), array( $inStr, stripcslashes( $inDiv ), $inPos ) ));
+	public static function runExplode_e( &$parser, $inStr = '', $inDiv = '', $inPos = 0, $inLim = null ) {
+		return PF::runExplode( $parser, $inStr, stripcslashes( $inDiv ), $inPos, $inLim );
 	}
 
 	/**
